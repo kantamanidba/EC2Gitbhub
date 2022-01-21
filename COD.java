ec9c72d523a9c456e31a23e61f829f1873ec1354
kantamanidba <narayanakdba@gmail.com>
2022-01-20 22:03:50 -0500

    COD Implemented

--- COD.java
+++ COD.java
@@ -1,5 +1,6 @@
 class COD {  
     public static void main(String args[]){  
      System.out.println("Implement cash on delivery");  
+     System.out.println("Implement cash on delivery new");
     }  
 } 
